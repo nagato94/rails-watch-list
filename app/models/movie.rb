052afbc7778b9e28b79bbeda16e6c12569968942
Kaique Costa <kaiquenagato94@outlook.com>
2023-08-25 15:04:38 -0300

    cloudinary add

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-
+  has_one_attached :photo
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
   validate :check_for_bookmarks_before_destroy
